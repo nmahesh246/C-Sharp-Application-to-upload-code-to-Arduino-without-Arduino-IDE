94aedd79cb10b04063ea6a54ad1f100c4e78ab0c
Mahesh N <mahesh.n246@gmail.com>
2018-05-22 07:59:34 +0530

    Add files via upload

---
+++ ArduinoUploader/ArduinoUploader/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ArduinoUploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboxComports = new System.Windows.Forms.ComboBox();
+            this.btnrefresh = new System.Windows.Forms.Button();
+            this.btnEraseSAM = new System.Windows.Forms.Button();
+            this.btnupload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboxComports
+            // 
+            this.comboxComports.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboxComports.FormattingEnabled = true;
+            this.comboxComports.Location = new System.Drawing.Point(39, 35);
+            this.comboxComports.Name = "comboxComports";
+            this.comboxComports.Size = new System.Drawing.Size(159, 21);
+            this.comboxComports.TabIndex = 3;
+            // 
+            // btnrefresh
+            // 
+            this.btnrefresh.Location = new System.Drawing.Point(228, 33);
+            this.btnrefresh.Name = "btnrefresh";
+            this.btnrefresh.Size = new System.Drawing.Size(159, 23);
+            this.btnrefresh.TabIndex = 2;
+            this.btnrefresh.Text = "Refresh Port";
+            this.btnrefresh.UseVisualStyleBackColor = true;
+            this.btnrefresh.Click += new System.EventHandler(this.btnrefresh_Click);
+            // 
+            // btnEraseSAM
+            // 
+            this.btnEraseSAM.Location = new System.Drawing.Point(228, 85);
+            this.btnEraseSAM.Name = "btnEraseSAM";
+            this.btnEraseSAM.Size = new System.Drawing.Size(159, 23);
+            this.btnEraseSAM.TabIndex = 4;
+            this.btnEraseSAM.Text = "Erase Flash";
+            this.btnEraseSAM.UseVisualStyleBackColor = true;
+            this.btnEraseSAM.Click += new System.EventHandler(this.btnEraseSAM_Click);
+            // 
+            // btnupload
+            // 
+            this.btnupload.Location = new System.Drawing.Point(39, 85);
+            this.btnupload.Name = "btnupload";
+            this.btnupload.Size = new System.Drawing.Size(159, 23);
+            this.btnupload.TabIndex = 54;
+            this.btnupload.Text = "Upload";
+            this.btnupload.UseVisualStyleBackColor = true;
+            this.btnupload.Click += new System.EventHandler(this.btnupload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 151);
+            this.Controls.Add(this.btnupload);
+            this.Controls.Add(this.btnEraseSAM);
+            this.Controls.Add(this.comboxComports);
+            this.Controls.Add(this.btnrefresh);
+            this.Name = "Form1";
+            this.Text = "Arduino Uploader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboxComports;
+        private System.Windows.Forms.Button btnrefresh;
+        private System.Windows.Forms.Button btnEraseSAM;
+        private System.Windows.Forms.Button btnupload;
+    }
+}
+
---
+++ ArduinoUploader/ArduinoUploader/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ArduinoUploader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnrefresh_Click(object sender, EventArgs e)
+        {
+            comboxComports.Items.Clear();
+            String[] portName = SerialPort.GetPortNames();
+            comboxComports.Items.AddRange(portName);
+            comboxComports.SelectedIndex = 0;
+        }
+
+        private void btnupload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboxComports.SelectedItem.ToString() != "")
+                {
+                    btnEraseSAM_Click(null, null);
+                    Thread.Sleep(2000);
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.Arguments = "/c bossac.exe -i -d --port=" + comboxComports.SelectedItem.ToString() + " -U false -e -w -b arduinocode.cpp.bin -R ";
+                    System.Diagnostics.Process.Start(startInfo);
+                    Thread.Sleep(15000);
+                    MessageBox.Show("Code Uploaded Successfully.");
+                }
+                else
+                    MessageBox.Show("Could not find COM Port, select COM port and re-run.");
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void btnEraseSAM_Click(object sender, EventArgs e)
+        {
+            SerialPort port = new SerialPort(comboxComports.SelectedItem.ToString(), 1200);
+            port.Open();
+            Thread.Sleep(1000);
+            port.Close();
+        }
+    }
+}
